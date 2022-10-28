f87f2b9a6488e061d82c30f89da60462938fe06e
kanya01 <kanyamosesm@gmail.com>
2022-10-28 11:24:52 +0100

    c5

--- Moses/mscript.js
+++ Moses/mscript.js
@@ -34,7 +34,7 @@
 const createElement = (x) => {
     //column
     var col = document.createElement('div');
-    col.className = 'col s3 s3';
+    col.className = 'col s3 m3';
 
     //card
     let card = document.createElement('div');
@@ -44,11 +44,10 @@ const createElement = (x) => {
 
     let cardImage = document.createElement('div')
     cardImage.className = 'card-image';
-
     card.appendChild(cardImage);
 
     let img = document.createElement('img');
-    img.src = ``
+    img.src = `https://image.tmdb.org/t/p/w1280${x.poster_path}`;
     cardImage.appendChild(img);
 
     let overlay = document.createElement('div');
@@ -79,52 +78,86 @@ const createElement = (x) => {
 
 
 }
- //display shows
-const getTvShows = () => {
-    fetch(`https://api.tvmaze.com/singlesearch/shows?q="${show}`)
-    .then(response => response.json())
-    .then(data =>{ 
-        data.results.forEach(element => {
-            createElements(element)
-        });
-        setTimeout(() => {
-            document.getElementById('lds-roller').style = "display:none";
-        }, 10000);
-    
-});
-};
-
-//call on window
-
-
-
-//search a show by entering a name
-
-const fetchName=()=>{
-    let value = input.value;
-    fetch(`https://api.tvmaze.com/singlesearch/shows?q="${show}`)
-    .then(response => response.json())
-    .then(shows=>{
-        row_body.innerHTML="";
-        shows.results.forEach(element => {
-            createElements(element)
-        });
-
-       
-    })};
-
-/////////figure out how key element matches up with actual html file
-
-input.addEventListener("keyup", (e) => {
-    if(e.keyCode >= 48 && e.keyCode <= 90){
-        document.getElementById('key').innerHTML = e.key
-    }
-    if(e.keyCode === 13) {
-        e.preventDefault();
-        fetchName()
-    }
-})
  
-window.onload = function(){
-    getTvShows
-};
+ //first call to display random movies
+ /*
+ const options = {
+     method: 'GET',
+     headers: {
+         'X-RapidAPI-Key': 'ff5145fe65mshd8a7ba08d0a20d7p13b1c7jsnc9c35728ce0c',
+         'X-RapidAPI-Host': 'tvjan-tvmaze-v1.p.rapidapi.com'
+     }
+ };
+ */
+ /*const getShows=() =>{
+     fetch('https://tvjan-tvmaze-v1.p.rapidapi.com/shows/%7Bid%7D', options)
+     .then(response => response.json())
+     .then(data =>{
+         data.results.forEach(element => {
+             createElements(element)
+         })
+     })
+     .then(response => console.log(response))
+     .catch(err => console.error(err));
+ }
+ */
+ 
+ const getMovies=() =>{
+     fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=${Math.floor(Math.random() * 100) + 1}`)
+     .then(response => response.json())
+     .then(data =>{ 
+         data.results.forEach(element => {
+             createElements(element)
+         });
+         setTimeout(() => {
+             document.getElementById('lds-roller').style = "display:none";
+         }, 10000);
+     
+     });
+ 
+ }
+ 
+ //search for a show/movie
+ const fetchName=()=>{
+     let value = input.value;
+     fetch(`https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="${value}`)
+     .then(response => response.json())
+     .then(shows=>{
+         row_body.innerHTML="";
+         shows.results.forEach(element => {
+             createElements(element)
+         });
+ 
+      fetch('https://tvjan-tvmaze-v1.p.rapidapi.com/search/shows?q=%7Bquery%7D', options)  
+     })}
+ 
+ /*fetch('https://tvjan-tvmaze-v1.p.rapidapi.com/search/shows?q=%7Bquery%7D', options)
+     .then(response => response.json())
+     .then(response => console.log(response))
+     .then(shows=>{
+         row_body.innerHTML="";
+         shows.results.forEach(element => {
+             createElements(element)
+         });
+     })
+     .catch(err => console.error(err));
+ */
+ //search by enter key
+     input.addEventListener("keyup", function(event) {
+         console.log(event.key)
+         if(event.keyCode >= 48 && event.keyCode <= 90) {
+             document.getElementById('key').innerHTML = event.key
+         }        
+         if (event.keyCode === 13) {
+          event.preventDefault();
+          fetchName()
+         }
+       });
+ 
+   
+ 
+ 
+     window.onload = function() {
+         getMovies()
+       };
+       
